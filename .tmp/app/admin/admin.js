3e19219e5353cf17695c9b6e3b2d924de51a8120
ice-penguin <hanjian1106@163.com>
2016-04-02 09:28:49 +0800

    go

---
+++ .tmp/app/admin/admin.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('xinyixianApp').config(function ($routeProvider) {
+  $routeProvider.when('/admin', {
+    templateUrl: 'app/admin/admin.html',
+    controller: 'AdminCtrl'
+  });
+});
+//# sourceMappingURL=admin.js.map
---
+++ .tmp/app/main/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('xinyixianApp').config(function ($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'app/main/main.html',
+    controller: 'MainCtrl'
+  });
+});
+//# sourceMappingURL=main.js.map
