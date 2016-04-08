05f09940ae27a53f2e3865ab72458f7484f7c3ef
xt1994 <2500409635@qq.com>
2016-04-08 18:36:59 +0800

    article

--- client/app/show/home/home.controller.js
+++ client/app/show/home/home.controller.js
@@ -3,6 +3,10 @@
 angular.module('xinyixianApp')
   .controller('ViewHomeController', ['$state', '$stateParams', '$location', '$scope','$cookies', 'Upload',
     function ($state, $stateParams, $location, $scope,$cookies,Upload) {
-       
+       self = this;
+
+       self.go = function(){
+       	$state.go('list-view');
+       };
         
   }]);
--- client/app/show/show.routes.js
+++ client/app/show/show.routes.js
@@ -9,5 +9,12 @@ angular.module('xinyixianApp')
         controller: 'ViewHomeController',
         controllerAs: 'viewHomeCtrl',
         // authenticate: true
+      })
+      .state('list-view', {
+        url: '/show/list/view',
+        templateUrl: 'app/show/articlelist/articlelist.html',
+        controller: 'ViewListController',
+        controllerAs: 'viewListCtrl',
+        // authenticate: true
       });
   });
