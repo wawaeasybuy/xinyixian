52938c0fc231a80bab5913b38adde19dd08fd681
xt1994 <2500409635@qq.com>
2016-04-13 13:06:45 +0800

    ui

--- client/app/admin/image/add-image.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('xinyixianApp')
-  .controller('AddImageController', ['$state', '$stateParams', '$location', '$scope','$cookies', 'Article',
-    function ($state, $stateParams, $location, $scope,$cookies,Article) {
-    	var self=this;
-
-    
-  }]);
--- client/app/admin/image/edit-image.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('xinyixianApp')
-  .controller('EditImageController', ['$state', '$stateParams', '$location', '$scope','$cookies', 'Article',
-    function ($state, $stateParams, $location, $scope,$cookies,Article) {
-    	var self=this;
-
-    
-  }]);
